8164cf76bcd63275c89d6a59d5ee2661a221a44a
GamesWang <gameswang@gmail.com>
2016-07-15 22:27:12 +0800

    Add segmentation module for split Chinese words

---
+++ segmentation/segment.py
@@ -0,0 +1,72 @@
+#!/bin/usr/env python
+#-*- coding:utf-8 -*-
+
+import jieba
+import jieba.analyse
+from collections import Counter
+from module_config import *
+import re
+import json
+
+jieba.set_dictionary('./dicts/dict.txt.big') # set 繁體辭典
+jieba.load_userdict('./dicts/ptt_words.txt') # load PTT 用語
+jieba.load_userdict('./dicts/restaurant.txt') # load 餐廳名稱
+jieba.analyse.set_stop_words('./dicts/mystopwords.txt') # set stopwords
+
+# jieba.analyse.set_idf_path(file_name) # set idf
+
+# ret = open("president_speech.txt", "r", encoding='utf-8').read()
+# seglist = jieba.cut(ret, cut_all=False)
+# tags = jieba.analyse.extract_tags(content, 10) # get top 10 tf-idf
+
+# print(' '.join(seglist))
+
+# print(Counter(list(seglist)))
+
+def main():
+
+    parsed_count = 0
+    k_count = 0
+    ip_pat = re.compile('.+From: (((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)).*')
+
+    for doc in post_detail_infos.find({'segmented_title_and_body':{'$in':[None]}}):
+        parsed_count = (parsed_count + 1)%1000
+        if parsed_count == 0:
+            k_count += 1
+            print('Parsed: ' + str(k_count) + 'k docs')
+        
+        try:
+            post_ip = ''
+            m = ip_pat.match(doc['body_content'])
+            if m is not None:
+                post_ip = m.group(1)
+                doc['body_content'] = re.sub(post_ip, ' ', doc['body_content'])
+
+            body_seglist = jieba.cut(doc['body_content'], cut_all=False)
+            body_result = ' '.join(body_seglist)
+
+            title_seglist = jieba.cut(doc['title'], cut_all=False)
+            title_result = ' '.join(title_seglist)
+
+            update_data = {
+                'segmented_body': body_result + ' ' + post_ip,
+                'segmented_title': title_result,
+                'segmented_title_and_body': title_result + ' ' + body_result + ' ' + post_ip,
+                'jieba_top30_tfidf': jieba.analyse.extract_tags(doc['title'] + ' ' + doc['body_content'], 30),
+                'post_ip': post_ip,
+                'word_statistic': str(dict(Counter((title_result + ' ' + body_result + ' ' + post_ip).split())))
+            }
+            post_detail_infos.update_one({'_id': doc['_id']}, {'$set': update_data})
+
+            # for elem in update_data:
+            #     print(str(elem) + ': \n' + str(update_data[elem]))
+            #     print()
+
+        except Exception as e:
+            print('[Exception] post_url: ' + doc['post_url'] + str(e))
+            logRecord('[Exception] post_url: ' + doc['post_url'] + str(e), 'segment.log')
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
