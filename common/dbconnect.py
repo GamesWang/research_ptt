44acc971953203c4085cc5ce7e7cc12df60255f2
GamesWang <gameswang@gmail.com>
2016-07-15 17:14:59 +0800

    Add connect=False to MongoClient parameter

--- common/dbconnect.py
+++ common/dbconnect.py
@@ -5,7 +5,7 @@ import pymongo
 
 # mongoDB
 PTT_BOARD = 'Food_cleaned'
-client = pymongo.MongoClient('localhost', 27017)
+client = pymongo.MongoClient('localhost', 27017, connect=False)
 use_db = client['ptt_' + PTT_BOARD]
 post_url_infos = use_db['post_url_infos']
 post_detail_infos = use_db['post_detail_infos']
