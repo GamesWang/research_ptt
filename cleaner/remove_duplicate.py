87af7e988128005a205b97e1007fb8e4172429b0
GamesWang <gameswang@gmail.com>
2016-07-15 12:56:18 +0800

    Add a new script to remove documents with the same post_url

---
+++ cleaner/remove_duplicate.py
@@ -0,0 +1,24 @@
+#!/bin/usr/env python
+#-*- coding:utf-8 -*-
+
+from util import *
+from dbconnect import *
+
+def main():
+    pipeline = [
+        {"$group": {"_id": { "post_url": "$post_url"}, "dups": { "$push": "$_id" }, "count": {"$sum": 1}}},
+        {"$match": {"count": { "$gte": 2 }}},
+        {"$sort":{"count":-1}}
+    ]
+
+    for doc in post_detail_infos.aggregate(pipeline):
+        print('Remove: ' + doc['_id']['post_url'])
+        logRecord('Remove: ' + doc['_id']['post_url'], 'remove.log')
+        # print(doc['count'])
+        # print(doc['dups'][1:])
+
+        post_detail_infos.remove({'_id':{'$in':doc['dups'][1:]}})
+
+if __name__ == '__main__':
+    main()
+    exit(0)
