8d2d2113817682a78ab58339c007c7c9fe920366
GamesWang <gameswang@gmail.com>
2016-07-22 17:46:29 +0800

    Move dbconnect to common_config and fix related path

--- common/common_config.py
+++ common/common_config.py
@@ -5,8 +5,9 @@ import pymongo
 
 # mongoDB
 PTT_BOARD = 'Gossiping'
+DB_NAME = 'ptt_' + PTT_BOARD
 client = pymongo.MongoClient('localhost', 27017, connect=False)
-use_db = client['ptt_' + PTT_BOARD]
+use_db = client[DB_NAME]
 post_url_infos = use_db['post_url_infos']
 post_detail_infos = use_db['post_detail_infos']
 y2015_detail_infos = use_db['y2015_detail_infos']
